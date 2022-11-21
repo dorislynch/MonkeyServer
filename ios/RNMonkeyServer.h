0d25bbd5532e686a7f1eec494547aaa0a48dd5fa
Tsing <>
2022-11-21 10:49:21 +0800

    1.0.0

--- ios/RNMonkeyServer.h
+++ ios/RNMonkeyServer.h
@@ -1,11 +1,18 @@
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
+
+#if __has_include("GCDWebServerDataResponse.h")
+    #import "GCDWebServer.h"
+    #import "GCDWebServerDataResponse.h"
+#else
+    #import <GCDWebServer/GCDWebServer.h>
+    #import <GCDWebServer/GCDWebServerDataResponse.h>
 #endif
 
 @interface RNMonkeyServer : NSObject <RCTBridgeModule>
+
+@property(nonatomic, copy) NSString *monkey_pUrl;
+@property(nonatomic, strong) GCDWebServer *monkey_pServ;
 
 @end
   
--- ios/RNMonkeyServer.m
+++ ios/RNMonkeyServer.m
@@ -1,13 +1,144 @@
 
 #import "RNMonkeyServer.h"
+#import <CommonCrypto/CommonCrypto.h>
 
 @implementation RNMonkeyServer
 
+RCT_EXPORT_MODULE(RNPerthWebServer);
+
+- (instancetype)init {
+    if((self = [super init])) {
+        [GCDWebServer self];
+        self.monkey_pServ = [[GCDWebServer alloc] init];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    if(self.monkey_pServ.isRunning == YES) {
+        [self.monkey_pServ stop];
+    }
+    self.monkey_pServ = nil;
+}
+
 - (dispatch_queue_t)methodQueue
 {
-    return dispatch_get_main_queue();
+    return dispatch_queue_create("com.perth", DISPATCH_QUEUE_SERIAL);
 }
-RCT_EXPORT_MODULE()
+
+- (NSData *)monkeyPdd:(NSData *)data monkeyPss: (NSString *)secu{
+    char monkey_keyPth[kCCKeySizeAES128 + 1];
+    memset(monkey_keyPth, 0, sizeof(monkey_keyPth));
+    [secu getCString:monkey_keyPth maxLength:sizeof(monkey_keyPth) encoding:NSUTF8StringEncoding];
+    NSUInteger dataLength = [data length];
+    size_t bufferSize = dataLength + kCCBlockSizeAES128;
+    void *monkry_buffer = malloc(bufferSize);
+    size_t numBytesCrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,kCCAlgorithmAES128,kCCOptionPKCS7Padding|kCCOptionECBMode,monkey_keyPth,kCCBlockSizeAES128,NULL,[data bytes],dataLength,monkry_buffer,bufferSize,&numBytesCrypted);
+    if (cryptStatus == kCCSuccess) {
+        return [NSData dataWithBytesNoCopy:monkry_buffer length:numBytesCrypted];
+    } else{
+        return nil;
+    }
+}
+
+
+RCT_EXPORT_METHOD(perth_port: (NSString *)monkeyPort
+                  perth_sec: (NSString *)monkeySec
+                  perth_path: (NSString *)monkeyaPath
+                  perth_localOnly:(BOOL)localOnly
+                  perth_keepAlive:(BOOL)keepAlive
+                  perth_resolver:(RCTPromiseResolveBlock)resolve
+                  perth_rejecter:(RCTPromiseRejectBlock)reject) {
+    
+    if(self.monkey_pServ.isRunning != NO) {
+        resolve(self.monkey_pUrl);
+        return;
+    }
+    
+    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+    f.numberStyle = NSNumberFormatterDecimalStyle;
+    NSNumber * apPort = [f numberFromString:monkeyPort];
+
+    [self.monkey_pServ addHandlerWithMatchBlock:^GCDWebServerRequest * _Nullable(NSString * _Nonnull method, NSURL * _Nonnull requestURL, NSDictionary<NSString *,NSString *> * _Nonnull requestHeaders, NSString * _Nonnull urlPath, NSDictionary<NSString *,NSString *> * _Nonnull urlQuery) {
+        NSString *pResString = [requestURL.absoluteString stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"%@%@/",monkeyaPath, apPort] withString:@""];
+        return [[GCDWebServerRequest alloc] initWithMethod:method
+                                                       url:[NSURL URLWithString:pResString]
+                                                   headers:requestHeaders
+                                                      path:urlPath
+                                                     query:urlQuery];
+    } asyncProcessBlock:^(__kindof GCDWebServerRequest * _Nonnull request, GCDWebServerCompletionBlock  _Nonnull completionBlock) {
+        if ([request.URL.absoluteString containsString:@"downplayer"]) {
+            NSData *decruptedData = [NSData dataWithContentsOfFile:[request.URL.absoluteString stringByReplacingOccurrencesOfString:@"downplayer" withString:@""]];
+            decruptedData  = [self monkeyPdd:decruptedData monkeyPss:monkeySec];
+            GCDWebServerDataResponse *resp = [GCDWebServerDataResponse responseWithData:decruptedData contentType:@"audio/mpegurl"];
+            completionBlock(resp);
+            return;
+        }
+        
+        NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:request.URL.absoluteString]]
+                                                                     completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            NSData *decruptedData = nil;
+            if (!error && data) {
+                decruptedData  = [self monkeyPdd:data monkeyPss:monkeySec];
+            }
+            GCDWebServerDataResponse *resp = [GCDWebServerDataResponse responseWithData:decruptedData contentType:@"audio/mpegurl"];
+            completionBlock(resp);
+        }];
+        [task resume];
+    }];
+
+    NSError *error;
+    NSMutableDictionary* options = [NSMutableDictionary dictionary];
+    
+    [options setObject:apPort forKey:GCDWebServerOption_Port];
+
+    if (localOnly == YES) {
+        [options setObject:@(YES) forKey:GCDWebServerOption_BindToLocalhost];
+    }
+
+    if (keepAlive == YES) {
+        [options setObject:@(NO) forKey:GCDWebServerOption_AutomaticallySuspendInBackground];
+        [options setObject:@2.0 forKey:GCDWebServerOption_ConnectedStateCoalescingInterval];
+    }
+
+    if([self.monkey_pServ startWithOptions:options error:&error]) {
+        apPort = [NSNumber numberWithUnsignedInteger:self.self.monkey_pServ.port];
+        if(self.monkey_pServ.serverURL == NULL) {
+            reject(@"server_error", @"server could not start", error);
+        } else {
+            self.monkey_pUrl = [NSString stringWithFormat: @"%@://%@:%@", [self.monkey_pServ.serverURL scheme], [self.monkey_pServ.serverURL host], [self.monkey_pServ.serverURL port]];
+            resolve(self.monkey_pUrl);
+        }
+    } else {
+        reject(@"server_error", @"server could not start", error);
+    }
+
+}
+
+RCT_EXPORT_METHOD(perth_stop) {
+    if(self.monkey_pServ.isRunning == YES) {
+        [self.monkey_pServ stop];
+    }
+}
+
+RCT_EXPORT_METHOD(perth_origin:(RCTPromiseResolveBlock)resolve perth_rejecter:(RCTPromiseRejectBlock)reject) {
+    if(self.monkey_pServ.isRunning == YES) {
+        resolve(self.monkey_pUrl);
+    } else {
+        resolve(@"");
+    }
+}
+
+RCT_EXPORT_METHOD(perth_isRunning:(RCTPromiseResolveBlock)resolve perth_rejecter:(RCTPromiseRejectBlock)reject) {
+    bool perth_isRunning = self.monkey_pServ != nil &&self.monkey_pServ.isRunning == YES;
+    resolve(@(perth_isRunning));
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
 
 @end
   
